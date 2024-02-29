28683bb0f0f5081847df7f46598770b73589b028
Anatoly Dotsenko <docenkotolia004@gmail.com>
2024-02-29 23:59:20 +0300

    add third lab

--- Discrete math labs/lab1.cs
+++ Discrete math labs/lab1.cs
@@ -1,136 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Discrete_math_labs 
-{
-    class lab1
-    {
-        static void Main(string[] args)
-        {
-<<<<<<< HEAD
-            var multiplicity_U = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };     // универсальное множество U
-=======
-            var multiplicity_U = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };    // универсальное множество U
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//namespace Discrete_math_labs 
+//{
+//    class lab1
+//    {
+//        static void Main(string[] args)
+//        {
+//            var multiplicity_U = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };     // универсальное множество U
 
-            var multiplicity_A = new HashSet<string> { "1", "5", "6", "8" };    // множество А
-            var multiplicity_B = new HashSet<string> { "3", "6", "9", "10" };   // множество B
+//            var multiplicity_A = new HashSet<string> { "1", "5", "6", "8" };    // множество А
 
-            var union_multiplicity = new HashSet<string>(multiplicity_A);   // буфер множества A для объединения множеств
+//            var multiplicity_B = new HashSet<string> { "3", "6", "9", "10" };   // множество B
 
-            union_multiplicity.UnionWith(multiplicity_B);   // объединяем множество А с множеством В и записываем в отдельную переменную
+//            var union_multiplicity = new HashSet<string>(multiplicity_A);   // буфер множества A для объединения множеств
 
-            Console.Write("Объединенные множества А и В: ");
+//            union_multiplicity.UnionWith(multiplicity_B);   // объединяем множество А с множеством В и записываем в отдельную переменную
 
-            foreach (var element in union_multiplicity)
-            {
-                Console.Write($"{element} ");
-            }
+//            Console.Write("Объединенные множества А и В: ");
 
-            Console.WriteLine('\n');
+//            foreach (var element in union_multiplicity)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-<<<<<<< HEAD
-            var buffer_A = new HashSet<string>(multiplicity_A);    // буфер множества А для поиска пересечений с множеством В
+//            Console.WriteLine('\n');
 
-            var same = buffer_A.Intersect(multiplicity_B);  // поиск пересечений множества А с множеством В
-=======
-            var buffer_A = new HashSet<string> (multiplicity_A);    // буфер множества А для поиска пересечений с множеством В
+//            var buffer_A = new HashSet<string>(multiplicity_A);    // буфер множества А для поиска пересечений с множеством В
 
-            var same = buffer_A.Intersect(multiplicity_B);    // поиск пересечений множества А с множеством В
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//            var same = buffer_A.Intersect(multiplicity_B);  // поиск пересечений множества А с множеством В
 
-            Console.Write("Пересечение множеств А и В: ")
+//            Console.Write("Пересечение множеств А и В: ");
 
-            foreach (var element in same)
-            {
-                Console.Write($"{element} ");
-            }
+//            foreach (var element in same)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-<<<<<<< HEAD
-            var difference_A_buffer = new HashSet<string>(multiplicity_A);  // буфер множества А для вычисления разницы между множествами
+//            var difference_A_buffer = new HashSet<string>(multiplicity_A);  // буфер множества А для вычисления разницы между множествами
 
-            var difference_B_buffer = new HashSet<string>(multiplicity_B);  // буфер множества В для вычисления разницы между множествами
-=======
-            var difference_A_buffer = new HashSet<string>(multiplicity_A);    // буфер множества А для вычисления разницы между множествами
+//            var difference_B_buffer = new HashSet<string>(multiplicity_B);  // буфер множества В для вычисления разницы между множествами
 
-            var difference_B_buffer = new HashSet<string>(multiplicity_B);    // буфер множества В для вычисления разницы между множествами
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//            var difference_A_B = difference_A_buffer.Except(multiplicity_B);    // разность множества А и В
 
-            var difference_A_B = difference_A_buffer.Except(multiplicity_B);    // разность множества А и В
+//            var difference_B_A = difference_B_buffer.Except(multiplicity_A);    // разность множества В и А
 
-            var difference_B_A = difference_B_buffer.Except(multiplicity_A);    // разность множества В и А
+//            Console.WriteLine('\n');
 
-            Console.WriteLine('\n');
+//            Console.Write("Разность множеств А и В: ");
 
-            Console.Write("Разность множеств А и В: ");
+//            foreach (var element in difference_A_B)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-            foreach (var element in difference_A_B)
-            {
-                Console.Write($"{element} ");
-            }
+//            Console.WriteLine('\n');
 
-            Console.WriteLine('\n');
+//            Console.Write("Разность множеств B и A: ");
 
-            Console.Write("Разность множеств B и A: ");
+//            foreach (var element in difference_B_A)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-            foreach (var element in difference_B_A)
-            {
-                Console.Write($"{element} ");
-            }
+//            var symmetrical_difference = new HashSet<string>(difference_A_B.Union(difference_B_A)); // симметрическая разница множеств А и В
 
-<<<<<<< HEAD
-            var symmetrical_difference = new HashSet<string>(difference_A_B.Union(difference_B_A)); // симметрическая разница множеств А и В
-=======
-            var symmetrical_difference = new HashSet<string>(difference_A_B.Union(difference_B_A));    // симметрическая разница множеств А и В
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//            Console.WriteLine('\n');
 
-            Console.WriteLine('\n');
+//            Console.Write("Симметрическая разность A и B: ");
 
-            Console.Write("Симметрическая разность A и B: ");
+//            foreach (var element in symmetrical_difference)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-            foreach (var element in symmetrical_difference)
-            {
-                Console.Write($"{element} ");
-            }
+//            // дополненное множество А
 
-            // дополненное множество А
-<<<<<<< HEAD
-            var supplement_buffer_first = new HashSet<string>(multiplicity_U);  // первый буфер универсального множества U
-=======
-            var supplement_multiplicity_A = new HashSet<string>();  // пустое множество для дополнения множества А
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//            var supplement_buffer_first = new HashSet<string>(multiplicity_U);  // первый буфер универсального множества U
 
-            var supplement_multiplicity_A = supplement_buffer_first.Except(multiplicity_A); // дополняем множество А
+//            var supplement_multiplicity_A = supplement_buffer_first.Except(multiplicity_A);  // пустое множество для дополнения множества А
 
-            // дополненное множество В
-<<<<<<< HEAD
-            var supplement_buffer_second = new HashSet<string>(multiplicity_U); // второй буфер универсального множества U
-=======
-            var supplement_multiplicity_B = new HashSet<string>();    // пустое множество для дополнения множества В
->>>>>>> 434924dd1715baa543c6cccbb573ebb864ed5ddb
+//            // дополненное множество В
 
-            var supplement_multiplicity_B = supplement_buffer_second.Except(multiplicity_B);    // дополняем множество В
+//            var supplement_buffer_second = new HashSet<string>(multiplicity_U); // второй буфер универсального множества U
 
-            Console.WriteLine('\n');
+//            var supplement_multiplicity_B = supplement_buffer_second.Except(multiplicity_B);    // пустое множество для дополнения множества В
 
-            Console.Write("Дополненное множество А: ");
+//            Console.WriteLine('\n');
 
-            foreach (var element in supplement_multiplicity_A)
-            {
-                Console.Write($"{element} ");
-            }
+//            Console.Write("Дополненное множество А: ");
 
-            Console.WriteLine('\n');
+//            foreach (var element in supplement_multiplicity_A)
+//            {
+//                Console.Write($"{element} ");
+//            }
 
-            Console.Write("Дополненное множество B: ");
+//            Console.WriteLine('\n');
 
-            foreach (var element in supplement_multiplicity_B)
-            {
-                Console.Write($"{element} ");
-            }
-        }
-    }
-}
+//            Console.Write("Дополненное множество B: ");
+
+//            foreach (var element in supplement_multiplicity_B)
+//            {
+//                Console.Write($"{element} ");
+//            }
+//        }
+//    }
+//}
---
+++ Discrete math labs/lab2.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discrete_math_labs
+{
+    class lab2
+    {
+        public static void Main(string[] args)
+        {
+            int[,] BynaryMatrix = new int[5, 5] { { 1, 1, 0, 0, 1 },   // бинарная матрица P
+                                                   { 1, 1, 0, 0, 0 }, 
+                                                   { 0, 0, 1, 0, 0 }, 
+                                                   { 0, 0, 0, 1, 0 }, 
+                                                   { 1, 0, 0, 0, 0 } };
+
+            ReflexivityCheck(BynaryMatrix);    // проверка рефлексивности матрицы вызовом метода
+
+            int[,] TransposedMatrix = Transpose(BynaryMatrix);     // получаем транспонированную матрицу вызовом метода
+
+            SymmetryCheck(bynaryMatrix: BynaryMatrix, transposedMatrix: TransposedMatrix);  // проверка симметричности вызовом метода
+
+            AntisymmetricCheck(BynaryMatrix);   // проверки антисимметричности вызовом метода
+
+            TransitivityCheck(BynaryMatrix);
+        }
+
+        public static void ReflexivityCheck(int[,] bynaryMatrix)   // метод проверки рефлексивности бинарной матрицы
+        {
+            int rows = bynaryMatrix.GetLength(0);     // количество строк
+
+            int counter = 0;    // счетчик для подсчета случаев рефлексивности
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (bynaryMatrix[i, i] == 0)   // если главная диагональ содержит ноль
+                {
+                    Console.WriteLine("Бинарная матрица является антирефлексивной");    // выводим сообщение о антирефлексивности
+                }
+                else    // в ином случае...
+                {
+                    if (bynaryMatrix[i, i] == 1)   // если главная диагональ содержит единицу
+                    {
+                        counter++;  // увеличиваем счетчик на единицу
+                    }
+                }
+            }
+
+            if (counter == 5)   // если счетчик равен исходному количеству элементов в главной диагонали
+            {
+                Console.WriteLine("Бинарная матрица является рефлексивной");    // выводим сообщение о рефлексивности
+            }
+        }
+
+        public static int[,] Transpose(int[,] bynaryMatrix)   // метод для транспонирования бинарной матрицы
+        {
+            int rows = bynaryMatrix.GetLength(0);     // количество строк
+            int cols = bynaryMatrix.GetLength(1);     // количество столбцов
+
+            int[,] transposedMatrix = new int[rows, cols];  // пустая транспонированная матрица
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    transposedMatrix[j, i] = bynaryMatrix[i, j];  // запись в транспонированную матрицу элементов бинарной матрицы в обратном порядке
+                }
+            }
+
+            return transposedMatrix;    // возвращаем полученную матрицу
+        }
+
+        public static void SymmetryCheck(int[,] bynaryMatrix, int[,] transposedMatrix)  // метод проверки на симметричность или антисимметричность
+        {
+            int rows = bynaryMatrix.GetLength(0);     // количество строк
+            int cols = bynaryMatrix.GetLength(1);     // количество столбцов
+
+            bool flag = false;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (bynaryMatrix[i,j] == transposedMatrix[i,j])
+                    {
+                        flag = true;
+                        Console.WriteLine("Бинарная матрица симметрична");
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void AntisymmetricCheck(int[,] bynaryMatrix)
+        {
+            int rows = bynaryMatrix.GetLength(0);     // количество строк
+            int cols = bynaryMatrix.GetLength(1);     // количество столбцов
+
+            bool flag = false;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (bynaryMatrix[i, j] == 1 && bynaryMatrix[j, i] == 1 && i != j)
+                    {
+                        flag = true; 
+                        Console.WriteLine("Бинарная матрица антисимметрична");
+                        break;
+                    }
+                    else
+                    {
+                        flag = true;
+                        Console.WriteLine("Бинарная матрица не антисимметрична");
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void TransitivityCheck(int[,] bynaryMatrix)
+        {
+            int n = bynaryMatrix.GetLength(0);     // количество строк
+
+            bool flag = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (bynaryMatrix[i, j] == 1)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            if (bynaryMatrix[j, k] == 1 && bynaryMatrix[i, k] != 1)
+                            {
+                                flag = true;
+                                Console.WriteLine("Матрица не транзитивна");
+                                break;
+                            }
+                            else
+                            {
+                                flag = true;
+                                Console.WriteLine("Матрица транзитивна");
+                                break;
+                            }
+                        }
+                    }
+                    if (flag)
+                    {
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
